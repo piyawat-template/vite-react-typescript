15a50aba30001d80bafb48b74236674761ca484b
piyawat_nut <piyawat.su54@gmail.com>
2021-04-14 22:31:42 +0700

    Config: github flow to firebase hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from './assets/logo.svg'
+import './assets/css/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
